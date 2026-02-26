369d98a274f667569225427ddd12b16822738791
Johann3141592 <johann.posanski@gmail.com>
2026-02-26 15:31:12 +0000

    creating main.py and functions.py to dissolve the .ipynb structure

---
+++ main.py
@@ -0,0 +1,7 @@
+#This is the main file for the simulation. It imports the necessary libraries and functions and runs the simulation according to the setup.
+
+#Make sure to activate the conda environment before running this file.
+
+#getting the necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
